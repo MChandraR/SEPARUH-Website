a33827388c1789d33c6f6a032bbef3e592b6a738
MChandraR <2201020103@student.umrah.ac.id>
2024-09-11 22:57:21 +0700

    Buat server

--- server.js
+++ server.js
@@ -1,2 +1,27 @@
 const express = require('express');
-const {createServer} = require('http').createServer
+const createServer = require('http').createServer
+//Inisialisasi Server
+const app = express()
+const server = createServer(app, {});
+
+//Routing
+//Get untuk mengambil data dari server
+//Post biasany untuk mengirim data ke server
+//Put biasanya untuk update data
+//Delete delete data
+app.use(express.json());
+
+app.get('/', (req, res)=>{
+    res.send({
+        "status" : 200, 
+        "message" : "berhasil mengambil data",
+        "data" : {
+            "nama" : "Chandra", 
+            "nim" : "nim"
+        }
+    });
+});
+
+app.listen(3000, (e)=>{
+    console.log("Server start");
+});
