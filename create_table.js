8dc394f7372231d83e07b8474e41e6dca8720faf
SyawalN <135431644+SyawalN@users.noreply.github.com>
2024-09-18 22:43:57 +0700

    Created query example 2

--- create_table.js
+++ create_table.js
@@ -15,11 +15,11 @@ async function exportData() {
   try {
     await client.connect();
 
-    const result = await client.query('Create table users (user_id varchar (10), username varchar (50), password varchar (150), name varchar (50), role int, email varchar (50))');
-    const result2 = await client.query('Create table users (user_id varchar (10), username varchar (50), password varchar (150), name varchar (50), role int, email varchar (50))');
+    // const result = await client.query('Create table users (user_id varchar (10), username varchar (50), password varchar (150), name varchar (50), role int, email varchar (50))');
+    const result2 = await client.query('CREATE TABLE sessions ( session_id varchar(10), user_id varchar(10), token varchar(50), last_accessed timestamp, expired timestamp )');
 
-    console.log(result.rows);
-    console.log(result2.rows);
+    // console.log(result.rows);
+    // console.log(result2.rows);
   } catch (err) {
     console.error('Error saat mengekspor data:', err);
   } finally {
@@ -27,10 +27,4 @@ async function exportData() {
   }
 }
 
-console.log({
-    host: process.env.POSTGRES_HOST,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DATABASE,
-});
 exportData();
