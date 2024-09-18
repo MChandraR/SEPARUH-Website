5f2fab48963612eb04f6ea1bb2915e163b34102b
MChandraR <2201020103@student.umrah.ac.id>
2024-09-18 22:36:39 +0700

    Update:

--- create_table.js
+++ create_table.js
@@ -16,9 +16,10 @@ async function exportData() {
     await client.connect();
 
     const result = await client.query('Create table users (user_id varchar (10), username varchar (50), password varchar (150), name varchar (50), role int, email varchar (50))');
+    const result2 = await client.query('Create table users (user_id varchar (10), username varchar (50), password varchar (150), name varchar (50), role int, email varchar (50))');
 
-    const rows = result.rows;
-    console.log(rows);
+    console.log(result.rows);
+    console.log(result2.rows);
   } catch (err) {
     console.error('Error saat mengekspor data:', err);
   } finally {
@@ -28,8 +29,8 @@ async function exportData() {
 
 console.log({
     host: process.env.POSTGRES_HOST,
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DATABASE,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
 });
 exportData();
