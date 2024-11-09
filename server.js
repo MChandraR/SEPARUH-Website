98baf51c2c7f0582bbbe07a35502936b3ce10642
rasya266 <purnamarizki487@gmail.com>
2024-11-09 23:17:44 +0700

    Adding Store Session Feature to Mongo DB

--- server.js
+++ server.js
@@ -6,10 +6,21 @@ const utils = require('./utils/utils');
 const app = express()
 const path = require('path');
 const router = require('./app/routes');
+const mongoose = require('mongoose');
+const MongoStore = require('connect-mongo');
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@mydb.rvfulzg.mongodb.net/pemweb?retryWrites=true&w=majority&appName=myDB`;
+
+
 var sess = {
     secret: 'keyboard cat',
+    resave : false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+        mongoUrl: uri, 
+        collectionName: 'sessions'                      
+    }),
     cookie: {
-
+        maxAge: 1000 * 60 * 60 * 24, 
     }
 }
 
@@ -17,9 +28,6 @@ app.use(express.json());
 app.use(session(sess))
 app.use(express.static(path.join(__dirname, 'public')));
 app.use("/",router);
-
-
-
 
 app.get('/test', function(req, res, next) {
     if (req.session.views) {
